88f0864a492d66aba9c10e777c44ded72a60fd30
soleil <1938615010@qq.com>
2023-03-16 22:00:13 +0100

    rewrite kana card display function

--- main.js
+++ main.js
@@ -54,49 +54,124 @@ const ROMAJIS = [
 
 const KANAS = [HIRAGANAS, KATAKANAS, ROMAJIS]
 
+const kanaCards = []
+kanaCards[0] = document.querySelector('#hiragana-card')
+kanaCards[1] = document.querySelector('#katakana-card')
+kanaCards[2] = document.querySelector('#romaji-card')
+
+// current kana being learned, range from 0 to 75.
+// when the page is loaded at the first time, kanaIndex is set to -1 as the default value.
+let kanaIndex = -1
+
+// User's selection of kana types
+let kanaOptions = ['0', '1', '2']
+
 function toggleCard(event) {
   const btnID = this.id
   const cardID = btnID.substring(0, btnID.indexOf('-'))
-  const card = document.getElementById(cardID)
+  const card = document.getElementById(cardID + '-card')
   if (card.style.display === "none")
     card.style.display = "block"
   else
     card.style.display = "none"
 }
 
+/*
+ Once an user clicks the kanaCards UI component, this function will be invoked.
+ The process logic is as following:
+ 1. Clean the highlighted kanas (if kanaIndex > 0) in the kanaChart UI part;
+ 2. Randomly generate an integer ranged from 0 to 74 inclusive to represent the index of the kana to be learned;
+ 3. Highlight the kanas in kanaChart UI part;
+ 4. According to user's selection, display kana in one kanaCard UI part;
+*/
 function showKana() {
+  const kanaChartRows = document.querySelector('#kana-chart-tbl').rows
+
+  // 1. Clean highlighted kana cells
+  if (kanaIndex > 0) {
+    var kanaCells = locateKanaCells()
+    kanaCells.forEach(c => {
+      kanaChartRows[c.r].cells[c.c].style.border = "1px solid black"
+    })
+  }
+
+  // 2. Randomly generate a kana index
   const KANANUM = HIRAGANAS.length
-  let kanaIndex = 0;
   do {
     kanaIndex = Math.floor(Math.random() * KANANUM)
   } while (kanaIndex == 61 || kanaIndex == 63 || kanaIndex == 71 || kanaIndex == 73)
-  const kanaType = Math.floor(Math.random() * 3)
+
+  // 3. Highlight kana cells
+  kanaCells = locateKanaCells()
+  kanaCells.forEach(c => {
+    kanaChartRows[c.r].cells[c.c].style.border = "3px solid red"
+  })
+
+  // 4. Display kana card
+  // 4.1 Assign hiragana, katakana and romaji to kanaCards accordingly
   const hiragana = KANAS[0][kanaIndex]
   const katakana = KANAS[1][kanaIndex]
   const romaji = KANAS[2][kanaIndex]
 
-  const hiraganaCard = document.querySelector('#hiragana')
-  const katakanaCard = document.querySelector('#katakana')
-  const romajiCard = document.querySelector('#romaji')
+  kanaCards[0].innerHTML = hiragana
+  kanaCards[1].innerHTML = katakana
+  kanaCards[2].innerHTML = romaji
 
-  hiraganaCard.innerHTML = hiragana
-  katakanaCard.innerHTML = katakana
-  romajiCard.innerHTML = romaji
-
-  switch (kanaType) {
+  // 4.2 According to the user's selection, display only one kanaCard
+  let kanaType = 0
+  const kanaOptionsLength = kanaOptions.length
+  switch (kanaOptionsLength) {
     case 0:
-      katakanaCard.style.display = "none"
-      romajiCard.style.display = "none"
       break;
     case 1:
-      hiraganaCard.style.display = "none"
-      romajiCard.style.display = "none"
+      kanaType = parseInt(kanaOptions[0], 10)
       break;
     case 2:
-      hiraganaCard.style.display = "none"
-      katakanaCard.style.display = "none"
+      kanaType = parseInt(kanaOptions[Math.floor(Math.random() * 2)], 10)
+      break;
+    case 3:
+      kanaType = parseInt(kanaOptions[Math.floor(Math.random() * 3)], 10)
       break;
   }
+
+  for (let i = 0; i < kanaCards.length; i++) {
+    if (kanaType === i) {
+      kanaCards[i].style.display = "block"
+    } else {
+      kanaCards[i].style.display = "none"
+    }
+  }
+}
+
+function locateKanaCells() {
+  const r = kanaIndex % 5
+  const c = Math.trunc(kanaIndex / 5)
+  const hiraganaCell = { r: r + 1, c: 14 - c }
+  const katakanaCell = { r: r + 1 + 5, c: 14 - c }
+  const romajiCell = { r: r + 1 + 10, c: 14 - c }
+
+  const kanaCells = [hiraganaCell, katakanaCell, romajiCell]
+
+  return kanaCells
+}
+
+function selectKanaTypes(event) {
+  // console.log(`id:${event.target.id}, value:${event.target.value}, checked:${event.target.checked}`)
+
+  const v = event.target.value
+  const c = event.target.checked
+  if (c) {
+    if (kanaOptions.indexOf(v) === -1) {
+      kanaOptions.push(v)
+    }
+  } else {
+    let i = kanaOptions.indexOf(v)
+    if (i !== -1) {
+      kanaOptions.splice(i, 1)
+    }
+  }
+
+  // console.log(kanaOptions)
 }
 
 function fillKanaChartTbl(tbl, kanaArray) {
@@ -170,3 +245,7 @@ document.querySelector('#kana-cards-container').addEventListener('click', showKa
 
 generateKanaChart()
 document.querySelector('#dakuonToggle').addEventListener('input', toggleDakuon)
+
+document.querySelector('#hiraganaOption').addEventListener('input', selectKanaTypes)
+document.querySelector('#katakanaOption').addEventListener('input', selectKanaTypes)
+document.querySelector('#romajiOption').addEventListener('input', selectKanaTypes)
